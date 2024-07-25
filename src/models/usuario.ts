440b4466c29b3e47e9a039528351f2638b54add4
GEFR00 <elizabehtriquelme@gmail.com>
2024-07-25 19:41:51 -0400

    Módulo de usuarios

--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -4,11 +4,18 @@ export class Usuario {
     id: string;  
     nombre: string;  
     correoElectronico: string;  
-    contrasena: string;  
+    contrasena?: string;  
     direccion: string;  
     historialPedidos: Pedido[]; 
 
-    constructor(id: string, nombre: string, correoElectronico: string, contrasena: string, direccion: string, historialPedidos: Pedido[]) {
+    constructor(
+        id: string, 
+        nombre: string, 
+        correoElectronico: string, 
+        contrasena: string, 
+        direccion: string, 
+        historialPedidos: Pedido[]
+    ) {
         this.id = id;
         this.nombre = nombre;
         this.correoElectronico = correoElectronico;

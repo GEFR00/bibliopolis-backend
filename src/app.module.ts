5cf1e22fa71c221c26128e285d3c95f4fdf142fa
GEFR00 <elizabehtriquelme@gmail.com>
2024-07-25 21:24:41 -0400

    Módulo de Pedidos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,22 @@ import { UsuariosController } from './controllers/usuarios/usuarios.controller';
 import { UsuariosService } from './services/usuarios/usuarios.service';
 import { LibrosController } from './controllers/libros/libros.controller';
 import { LibrosService } from './services/libros/libros.service';
+import { PedidosController } from './controllers/pedidos/pedidos.controller';
+import { PedidosService } from './services/pedidos/pedidos.service';
 
 @Module({
   imports: [],
   controllers: [
     AppController,
     UsuariosController,
-    LibrosController
+    LibrosController,
+    PedidosController
   ],
   providers: [
     AppService,
     UsuariosService,
-    LibrosService
+    LibrosService,
+    PedidosService
   ],
 })
 export class AppModule {}
--- src/models/pedido.ts
+++ src/models/pedido.ts
@@ -4,12 +4,19 @@ import { Usuario } from "./usuario";
 export class Pedido {
     id: string;  
     usuario: Usuario;  
-    fechaPedido: Date;  
+    fechaPedido?: Date;  
     estado: 'pendiente' | 'en proceso' | 'enviado' | 'entregado'; 
     total: number;  
     items: ItemPedido[];  
 
-    constructor(id: string, usuario: Usuario, fechaPedido: Date, estado: 'pendiente' | 'en proceso' | 'enviado' | 'entregado', total: number, items: ItemPedido[]) {
+    constructor(
+        id: string, 
+        usuario: Usuario, 
+        fechaPedido: Date, 
+        estado: 'pendiente' | 'en proceso' | 'enviado' | 'entregado', 
+        total: number, 
+        items: ItemPedido[]
+    ) {
         this.id = id;
         this.usuario = usuario;
         this.fechaPedido = fechaPedido;
